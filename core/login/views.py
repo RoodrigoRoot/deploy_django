19d91f2e881e77b9480df903f3354007be6e5179
roodrigo <roodrigoroot@gmail.com>
2020-03-19 12:51:59 -0600

    commit

---
+++ core/login/views.py
@@ -0,0 +1,51 @@
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import FormView, RedirectView, TemplateView
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class LoginView(FormView):
+    form_class = AuthenticationForm
+    template_name = "login/login.html"
+    success_url = reverse_lazy("home")
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return super(LoginView, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return super(LoginView, self).form_valid(form)
+
+
+class LogoutView(RedirectView):
+    pattern_name = 'login'
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return super(LogoutView, self).get(request, *args, **kwargs)
+
+
+class HopmeView(TemplateView):
+    template_name = 'base/panel.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(HopmeView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = True
+        return context
+
+from django.views.defaults import page_not_found
+
+def mi_error_404(request):
+    nombre_template = 'base/404.html'
+ 
+    return page_not_found(request, template_name=nombre_template)
